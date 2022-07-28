0a31f90429dbe452ff7f7b3ef54c503cde3e5969
Arif5577 <arifalamm33@gmail.com>
2022-07-28 10:43:23 +0600

    modal component added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,2 @@
 import "flowbite";
+import * as icons from "phosphor-icons";
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*html", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#289BDE",
+        darkOne: "#1d1d1d",
+        darkTwo: "#4A4A4A",
+        darkThree: "#777",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
